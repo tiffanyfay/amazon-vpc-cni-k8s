62bca1738e28a446ac6ddb62ce315c0d648a0179
tiffany jernigan <tfj@amazon.com>
2019-06-07 21:07:42 +0000

    Update e2e script with blocking

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -4,14 +4,20 @@ set -ux
 IFS=$'\n\t'
 
 TEST="$1"
+NO_EXIT="$2"
+
 OUT_PATH="/tmp/cni-e2e"
 LOG_PATH="$OUT_PATH/plugins/e2e/results"
 
 mkdir -p "$LOG_PATH"
 
 ginkgo -v "$TEST" | tee "$LOG_PATH/e2e.log"
-STATUS=$?
 
 tar -C "$OUT_PATH" -cvzf "$OUT_PATH/results.tar.gz" plugins/
 
-exit $STATUS
+if [ "$NO_EXIT" == true ]; then
+    echo 'no-exit was specified, cni-e2e is now blocking'
+    trap : TERM INT; sleep infinity & wait
+fi
+
+
